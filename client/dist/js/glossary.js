e4b4d5e66d8e3d94d1ba41541e529d6744ce06f6
Adrian Jimson <adrian.jimson@silverstripe.com>
2022-01-07 13:45:31 +1300

    Fixing template and updating shortcode parser

--- client/dist/js/glossary.js
+++ client/dist/js/glossary.js
@@ -1,52 +1,168 @@
-const shortcodeAttrPattern = /[,\s]+([a-z0-9\-_]+)=(?:([a-z0-9\-_]+)|(\d+\.\d+)|(?:'([^']*)')|(?:"([^"]*)"))/;
 /**
- * Parse the shortcode properties
- * @param input The properties in shortcode need to be parsed
- * @returns {{}}
+ * The code of this shortCodeParse helper is mostly copied from
+ * https://github.com/silverstripe/silverstripe-admin/blob/1/client/src/lib/ShortcodeSerialiser.js
  */
-const parseProperties = (input) => {
-  let unmatched = input;
-  const result = {};
-  let match = unmatched.match(shortcodeAttrPattern);
-  while (match) {
-    // Save key / value
-    const key = match[1] || "";
-    const value = match[2] || match[3] || match[4] || match[5] || "";
-    if (key) {
-      result[key] = value;
+// Used to match outer regexp and get attrs as a string
+// All attrs extracted into matches[1]
+const stringifyRegex = (regexp) => (regexp.toString().slice(1, -1));
+const SHORTCODE_ATTRS = stringifyRegex(
+  /((?:[,\s]+(?:[a-z0-9\-_]+)=(?:(?:[a-z0-9\-_]+)|(?:\d+\.\d+)|(?:'[^']*')|(?:"[^"]*")))*)/
+);
+// Used to extract individual items from above regexp
+// Each item matches[1] is key, and matches[2] || matches[3] || matches[4] || matches[5] is value
+// eslint-disable-next-line max-len
+const SHORTCODE_ATTR = /[,\s]+([a-z0-9\-_]+)=(?:([a-z0-9\-_]+)|(\d+\.\d+)|(?:'([^']*)')|(?:"([^"]*)"))/;
+const SHORTCODE_OPEN = stringifyRegex(/\[%s/);
+const SHORTCODE_RIGHT_BRACKET = '\\]';
+const SHORTCODE_CLOSE = stringifyRegex(/\[\s*\/\s*%s\s*]/);
+const SHORTCODE_CONTENT = stringifyRegex(/((?:.|\n|)*?)/);
+const SHORTCODE_SPACE = stringifyRegex(/\s*/);
+
+/**
+ * Substitutes %s with parameter
+ * given in list. %%s is used to escape %s.
+ *
+ * @param {String} - The string to perform the substitutions on.
+ * @return {String} - The new string with substitutions made.
+ */
+const sprintf = (s, param) => {
+  if (!param) {
+    return s;
+  }
+
+  const regx = new RegExp('(.?)(%s)', 'g');
+
+  return s.replace(regx, (match, subMatch1) => {
+    // skip %%s
+    if (subMatch1 === '%') {
+      return match;
     }
 
-    // Trim off matched content from unmatched and continue parsing
-    const idx = unmatched.indexOf(match[0]);
-    unmatched = unmatched.substr(idx + match[0].length);
-    match = unmatched.match(shortcodeAttrPattern);
-  }
-  return result;
+    return subMatch1 + param;
+  });
+}
+
+const shortCodeParser = {
+  /**
+   * Matches the next occurance of a shortcode in a string.
+   *
+   * Returns object with keys:
+   *  - name (tag name)
+   *  - original (original matched string)
+   *  - properties (key-value pair of properties)
+   *  - content (between open / close tags)
+   *  - wrapped (bool flag)
+   *
+   * @param {String} name - shortcode tag
+   * @param {Boolean} wrapped - Expect a closing tag? ([tag][/tag]) or simple tag ([tag])
+   * @param {String} content - string to parse
+   * @return {Object} Object, or null if not found
+   */
+  match(name, wrapped, content) {
+    // Build matching regexp
+    const open = sprintf(SHORTCODE_OPEN, name);
+    let pattern = `${open}${SHORTCODE_ATTRS}${SHORTCODE_SPACE}${SHORTCODE_RIGHT_BRACKET}`;
+    if (wrapped) {
+      pattern = `${pattern}${SHORTCODE_CONTENT}${sprintf(SHORTCODE_CLOSE, name)}`;
+    }
+
+    // Get next match
+    const regex = new RegExp(pattern, 'i');
+    const match = regex.exec(content);
+    if (!match) {
+      return null;
+    }
+
+    // parse attributes
+    const properties = this.parseProperties(match[1]);
+    return {
+      name,
+      wrapped,
+      properties,
+      original: match[0],
+      content: wrapped ? match[2] : null,
+    };
+  },
+
+  /**
+   * Parse shortcode props from string
+   *
+   * @param {String} input
+   * @return {Object}
+   */
+  parseProperties(input) {
+    let unmatched = input;
+    const result = {};
+    let match = unmatched.match(SHORTCODE_ATTR);
+    while (match) {
+      // Save key / value
+      const key = match[1] || '';
+      const value = match[2] || match[3] || match[4] || match[5] || '';
+      if (key) {
+        result[key] = value;
+      }
+
+      // Trim off matched content from unmatched and continue parsing
+      const idx = unmatched.indexOf(match[0]);
+      unmatched = unmatched.substr(idx + match[0].length);
+      match = unmatched.match(SHORTCODE_ATTR);
+    }
+    return result;
+  },
+
+  /**
+   * Turn shortcode object into string.
+   * Note that if a shortcode is placed into a html attribute, use attributesafe to true to
+   * use attribute protected characters. For example:
+   *  - [sitetree_link id="3"] (attributesafe = false)
+   *  - [sitetree_link,id='3'] (attributesafe = true)
+   *
+   * Note that special characters (e.g. quotes) will be stripped if they would otherwise
+   * break the shortcode.
+   *
+   * @param {Object} object - Object in same format as match() (original ignored)
+   * @param {Boolean} attributesafe - Set to true to encode in attribute safety mode
+   * @return {String}
+   */
+  serialise(object, attributesafe = false) {
+    // attributesafe can only encode alphanumeric characters
+    const rule = attributesafe
+      ? { sep: ',', quote: '', replacer: /[^a-z0-9\-_.]/gi }
+      : { sep: ' ', quote: '"', replacer: /"/g };
+    const attrs = Object.entries(object.properties)
+      .map(([name, value]) => ((value)
+          ? `${rule.sep}${name}=${rule.quote}${`${value}`.replace(rule.replacer, '')}${rule.quote}`
+          : null
+      ))
+      .filter((attr) => attr !== null)
+      .join('');
+
+    if (object.wrapped) {
+      return `[${object.name}${attrs}]${object.content}[/${object.name}]`;
+    }
+    return `[${object.name}${attrs}]`;
+  },
 };
 
-/**
- * Match the [glossary_term] shortcode
- * @param content The content from the active editor
- * @returns Properties and content in the shortcode, and the original shortcode
- */
-const matchShortcode = (content) => {
-  // Build matching regexp
-  const pattern = /\[glossary_term\sid="[0-9]+"\]([A-Za-z0-9\.\-\s_]+)\[\/glossary_term\]/; // eslint-disable-line
+const createHTMLSanitiser = () => {
+  const div = document.createElement('div');
+  return (str) => {
+    if (str === undefined) {
+      return '';
+    }
 
-  // Get next match
-  const regex = new RegExp(pattern, "i");
-  const match = regex.exec(content);
-  if (!match) {
-    return null;
-  }
+    div.textContent = str;
 
-  // Parse properties in the shortcode
-  const properties = parseProperties(match[0]);
-  return {
-    properties,
-    textContent: match[1],
-    original: match[0],
+    return div.innerHTML;
   };
+};
+
+const sanitiseShortCodeProperties = (rawProperties) => {
+  const sanitise = createHTMLSanitiser();
+  return Object.entries(rawProperties).reduce((props, [name, value]) => ({
+    ...props,
+    [name]: sanitise(value)
+  }), {});
 };
 
 /**
@@ -107,13 +223,14 @@ const matchShortcode = (content) => {
     editor.addCommand("ssglossary", () => {
       editor.setProgressState(1);
       // Fetch the glossary data. If it succeeds, open the Glossary modal, otherwise, show error alert
-      fetch("http://adrian.sandbox/api/glossary")
+      fetch("./api/glossary")
         .then(
           (response) => {
             if (response.status !== 200) {
               editor.setProgressState(0);
               throw new Error(`Failed to fetch glossary.`);
             }
+
             editor.setProgressState(0);
             return response.json();
           }
@@ -133,7 +250,7 @@ const matchShortcode = (content) => {
      */
     editor.on("SaveContent", (o) => {
       const parser = new DOMParser();
-      const content = parser.parseFromString(`<div>${o.content}</div>`, "text/xml");
+      const content = parser.parseFromString(o.content, "text/html");
       const filter = `span[data-shortcode="glossary_term"]`;
       const elementList = content.querySelectorAll(filter);
 
@@ -142,16 +259,25 @@ const matchShortcode = (content) => {
       }
 
       elementList.forEach((element) => {
-        if (element.length === 0) {
+        if (!element.innerHTML) {
           return;
         }
 
         const termID = element.getAttribute("data-id");
-        const shortcode = `[glossary_term id="${termID}"]${element.innerHTML}[/glossary_term]`;
+        const properties = sanitiseShortCodeProperties({
+          id: termID
+        });
+        // const shortcode = `[glossary_term id="${termID}"]${element.innerHTML}[/glossary_term]`;
+        const shortcode = shortCodeParser.serialise({
+          name: 'glossary_term',
+          properties,
+          wrapped: true,
+          content: element.innerHTML
+        });
         element.replaceWith(shortcode);
       });
 
-      o.content = content.firstChild.innerHTML;
+      o.content = content.body.innerHTML;
     });
 
     /**
@@ -163,14 +289,14 @@ const matchShortcode = (content) => {
     editor.on("BeforeSetContent", (o) => {
       let {content} = o;
       // Match [glossary_term] shortcodes
-      let match = matchShortcode(content);
+      let match = shortCodeParser.match('glossary_term', true, content);
       // Transform the shortcodes to html '<span>...</span>'
       while (match) {
-        const { original, properties, textContent } = match;
+        const { original, properties } = match;
         // Transform the shortcode to raw html
-        const raw = `<span data-shortcode="glossary_term" data-id="${properties.id}">${textContent}</span>`;
+        const raw = `<span data-shortcode="glossary_term" data-id="${properties.id}">${match.content}</span>`;
         content = content.replace(original, raw);
-        match = matchShortcode(content);
+        match = shortCodeParser.match('glossary_term', true, content);
       }
       o.content = content;
     });
--- templates/TheSceneman/SilverStripeGlossary/View/GlossaryShortcodeProvider.ss
+++ templates/TheSceneman/SilverStripeGlossary/View/GlossaryShortcodeProvider.ss
@@ -1,3 +1,3 @@
 <% require css('thesceneman/silverstripe-glossary:client/dist/styles/bundle.css') %>
-<span class="inline-glossary-term">$Content</span>
-<div class="inline-glossary-definition">$Terminology</div>
+<span class="inline-glossary-term">$Content<span class="inline-glossary-definition">$Terminology</span></span>
+
